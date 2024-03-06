d493235d164c66a8960b795fb571c92339b6d6d3
ihaback <ihaback@fastmail.com>
2024-03-06 12:28:15 +0100

    possible solution

--- src/Select.test.js
+++ src/Select.test.js
@@ -1,8 +1,21 @@
 import TestRenderer from "react-test-renderer";
 import { AriaSelect } from "./Select";
+import ReactDOM from "react-dom";
 
-test("generating snapshot for Select", () => {
-  const testRenderer = TestRenderer.create(<AriaSelect />);
+describe("Select", () => {
+  beforeAll(() => {
+    ReactDOM.createPortal = jest.fn((element, node) => {
+      return element;
+    });
+  });
 
-  expect(testRenderer).toMatchSnapshot();
+  afterEach(() => {
+    ReactDOM.createPortal.mockClear();
+  });
+
+  test("generating snapshot for Select", () => {
+    const testRenderer = TestRenderer.create(<AriaSelect />);
+
+    expect(testRenderer).toMatchSnapshot();
+  });
 });
